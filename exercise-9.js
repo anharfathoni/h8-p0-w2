fc18d4cf3fd9994b65e5b89df0de7be8e4be56d0
Anhar <anharqwerty@gmail.com>
2018-09-26 12:31:08 +0700

    add exercise files

--- exercise-9.js
+++ exercise-9.js
@@ -27,10 +27,10 @@ function processSentence(a, b, c, d) {
     return full
 }
 
-var name = "Agus";
-var age = 30;
-var address = "Jln. Malioboro, Yogjakarta";
-var hobby = "gaming";
+var name = "Anhar";
+var age = 22;
+var address = "Jln. ABC , Indonesia";
+var hobby = "ngoding";
 
 var fullSentence = processSentence(name, age, address, hobby);
-console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
+console.log(fullSentence);
